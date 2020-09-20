0bd930cdd8292a0fb44ba218b58fc4f628d77148
pradeep chabukswar <pradeep.chabukswar@gmail.com>
2020-09-20 19:18:35 +0530

    Created Cart with Customer Type and Total Amount

--- src/main/java/com/in/cart/ShoppingCart.java
+++ src/main/java/com/in/cart/ShoppingCart.java
@@ -1,4 +1,12 @@
 package com.in.cart;
 public class ShoppingCart {
 
+    private final String customerType;
+    private final double totalAmount;
+
+    public ShoppingCart(String customerType, double totalAmount) {
+
+        this.customerType = customerType;
+        this.totalAmount = totalAmount;
+    }
 }
--- src/test/java/com/in/cart/ShoppingCartTest.java
+++ src/test/java/com/in/cart/ShoppingCartTest.java
@@ -1,7 +1,19 @@
 package com.in.cart;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ShoppingCartTest {
 
+    @Test
+    public void shouldAddShoppingCartWithCustomerTypeAndTotalAmount() {
+        ShoppingCart shoppingCart=new ShoppingCart("REGULAR",5000);
+        assertNotNull(shoppingCart);
+    }
+
+    @Test
+    public void name() {
+
+    }
 }
