ca0651da811694fca1e1707545af47db3c91ac8f
pradeep chabukswar <pradeep.chabukswar@gmail.com>
2020-09-20 21:21:58 +0530

    What-----  - Added Premium Customer Type as Base Information  - Refactored test cases to accommodate new Customer Type

---
+++ src/main/java/com/in/cart/customer/type/PremiumCustomer.java
@@ -0,0 +1,15 @@
+package com.in.cart.customer.type;
+
+public class PremiumCustomer implements CustomerType {
+
+    private String name;
+
+    public PremiumCustomer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getCustomerType() {
+        return name;
+    }
+}
--- src/test/java/com/in/cart/ShoppingCartTest.java
+++ src/test/java/com/in/cart/ShoppingCartTest.java
@@ -1,6 +1,7 @@
 package com.in.cart;
 
 import com.in.cart.customer.type.CustomerType;
+import com.in.cart.customer.type.PremiumCustomer;
 import com.in.cart.customer.type.RegularCustomer;
 import com.in.cart.promotion.Promotion;
 import com.in.cart.promotion.PromotionEngine;
@@ -17,12 +18,15 @@ import static org.junit.Assert.*;
 public class ShoppingCartTest {
 
     public static final String REGULAR = "Regular";
+    public static final String PREMIUM = "Premium";
 
     ShoppingCart shoppingCart;
     CustomerType regularCustomer;
+    CustomerType premiumCustomer;
 
     @Before
     public void setUp() throws Exception {
+        premiumCustomer = new PremiumCustomer(PREMIUM);
         regularCustomer = new RegularCustomer(REGULAR);
         shoppingCart = new ShoppingCart(regularCustomer, 5000);
     }
@@ -39,7 +43,7 @@ public class ShoppingCartTest {
     }
 
     @Test
-    public void shouldBeAbleToApplyDiscountBasedOnCustomerType() {
+    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Regular() {
         Map<CustomerType, List<Promotion>> promotions = new HashMap();
         Promotion promotion = new Promotion(0, 5000, 0);
         promotions.put(regularCustomer, Arrays.asList(promotion));
@@ -49,7 +53,7 @@ public class ShoppingCartTest {
     }
 
     @Test
-    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Scenario2_WithOrderValue10000() {
+    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Regular_Scenario2_WithOrderValue10000() {
         shoppingCart = new ShoppingCart(regularCustomer, 10000);
         Map<CustomerType, List<Promotion>> promotions = new HashMap();
         Promotion promotion = new Promotion(0, 5000, 0);
@@ -61,7 +65,7 @@ public class ShoppingCartTest {
     }
 
     @Test
-    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Scenario3_WithOrderValue5000() {
+    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Regular_Scenario3_WithOrderValue5000() {
         Map<CustomerType, List<Promotion>> promotions = new HashMap();
         Promotion promotion = new Promotion(0, 5000, 0);
         Promotion promotion2 = new Promotion(5000, 10000, 10);
@@ -72,28 +76,48 @@ public class ShoppingCartTest {
     }
 
     @Test
-    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Scenario4_WithOrderValue15000() {
+    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Regular_Scenario4_WithOrderValue15000() {
         shoppingCart = new ShoppingCart(regularCustomer, 15000);
         Map<CustomerType, List<Promotion>> promotions = new HashMap();
-        Promotion promotion = new Promotion(0, 5000, 0);
-        Promotion promotion2 = new Promotion(5000, 10000, 10);
-        Promotion promotion3 = new Promotion(10000, Double.MAX_VALUE, 20);
-        promotions.put(regularCustomer, Arrays.asList(promotion, promotion2, promotion3));
+        preparePromotionsForRegularCustomer(promotions);
         PromotionEngine promotionEngine = new PromotionEngine(promotions);
         shoppingCart.setPromotionEngine(promotionEngine);
         assertEquals(13500, shoppingCart.calculateTotal(), 0.001);
     }
 
     @Test
-    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Scenario5_WithOrderValue10000() {
+    public void shouldBeAbleToApplyDiscountBasedOnCustomerType_Regular_Scenario5_WithOrderValue10000() {
         shoppingCart = new ShoppingCart(regularCustomer, 10000);
         Map<CustomerType, List<Promotion>> promotions = new HashMap();
-        Promotion promotion = new Promotion(0, 5000, 0);
-        Promotion promotion2 = new Promotion(5000, 10000, 10);
-        Promotion promotion3 = new Promotion(10000, Double.MAX_VALUE, 20);
-        promotions.put(regularCustomer, Arrays.asList(promotion, promotion2, promotion3));
+        preparePromotionsForRegularCustomer(promotions);
         PromotionEngine promotionEngine = new PromotionEngine(promotions);
         shoppingCart.setPromotionEngine(promotionEngine);
         assertEquals(9500, shoppingCart.calculateTotal(), 0.001);
     }
+
+    private void preparePromotionsForRegularCustomer(Map<CustomerType, List<Promotion>> promotions) {
+        Promotion promotion = new Promotion(0, 5000, 0);
+        Promotion promotion2 = new Promotion(5000, 10000, 10);
+        Promotion promotion3 = new Promotion(10000, Double.MAX_VALUE, 20);
+        promotions.put(regularCustomer, Arrays.asList(promotion, promotion2, promotion3));
+    }
+
+    private void preparePromotionsForPremiumCustomer(Map<CustomerType, List<Promotion>> promotions) {
+        Promotion promotion = new Promotion(0, 4000, 10);
+        Promotion promotion2 = new Promotion(4000, 8000, 15);
+        Promotion promotion3 = new Promotion(8000, 12000, 20);
+        Promotion promotion4 = new Promotion(10000, Double.MAX_VALUE, 30);
+        promotions.put(premiumCustomer, Arrays.asList(promotion, promotion2, promotion3,promotion4));
+    }
+
+    @Test
+    public void shouldAbleToAddNewCustomerType_Premium_WithCorrespondingPromotions_Scenario1() {
+        shoppingCart = new ShoppingCart(premiumCustomer, 4000);
+        Map<CustomerType, List<Promotion>> promotions = new HashMap();
+        preparePromotionsForPremiumCustomer(promotions);
+        PromotionEngine promotionEngine = new PromotionEngine(promotions);
+        shoppingCart.setPromotionEngine(promotionEngine);
+        assertEquals(3600, shoppingCart.calculateTotal(), 0.001);
+    }
+
 }
