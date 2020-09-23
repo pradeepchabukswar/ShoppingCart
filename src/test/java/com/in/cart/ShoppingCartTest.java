8ef7b96fd4e1a388684dcebbbc039f55bc439f94
Pradeep Chabukswar <pradeep.chabukswar@gmail.com>
2020-09-23 12:10:20 +0530

    Test Commit

--- src/test/java/com/in/cart/ShoppingCartTest.java
+++ src/test/java/com/in/cart/ShoppingCartTest.java
@@ -148,6 +148,7 @@ public class ShoppingCartTest {
         PromotionEngine promotionEngine = new PromotionEngine(promotions);
         shoppingCart.setPromotionEngine(promotionEngine);
         assertEquals(15800, shoppingCart.calculateTotal(), 0.001);
+
     }
 
 }
